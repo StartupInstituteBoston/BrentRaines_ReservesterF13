8247a4702f68a9f0b314c584cadb5bd8364ef5b2
Brent Raines <bt.raines@gmail.com>
2013-11-07 11:05:23 -0500

    move dotenv in app.rb

--- config/application.rb
+++ config/application.rb
@@ -2,15 +2,15 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-require 'dotenv'
-Dotenv.load
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
 module Reservester
   class Application < Rails::Application
+
+    require 'dotenv'
+    Dotenv.load
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
