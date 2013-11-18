20760ca00abaa535b1af85c646d3b6c5ba4f67b9
Brent Raines <bt.raines@gmail.com>
2013-11-18 12:51:50 -0500

    re-arrange environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,3 @@
-# Load the Rails application.
-require File.expand_path('../application', __FILE__)
-
 ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
@@ -10,6 +7,9 @@ ActionMailer::Base.smtp_settings = {
   :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }
+
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Reservester::Application.initialize!
